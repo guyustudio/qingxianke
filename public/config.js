827b4dd9eeeb9ce44bfa17d727234740ffda7465
hujin <hujin685@jk.cn>
2014-10-26 18:48:21 +0800

    引入bootstrap&supervisor 完成头部静态html

--- public/config.js
+++ public/config.js
@@ -1,10 +1,11 @@
 requirejs.config({
     baseUrl: '/scripts',
     paths: {
-        jquery: '/bower_components/jquery/dist/jquery.min',
-        underscore: '/bower_components/underscore/underscore'
+        jquery: '/bower_components/jquery/dist/jquery',
+        underscore: '/bower_components/underscore/underscore',
+        bootstrap: '/bower_components/bootstrap/js/bootstrap'
     }
 });
 
 //全局加载通用模块
-requirejs(['jquery', 'underscore']);
+requirejs(['jquery', 'underscore', 'bootstrap']);
